4af5980467349be8faea36ad228e1ad962f0f77f
vishwaroopa <vishwarupa.vishu@gmail.com>
2022-02-05 15:23:07 +0530

    'index'

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,26 +1,31 @@
 <style scoped>
-   @media only screen and (max-width: 600px) {
+    @media only screen and (max-width: 600px) {
         .announcement-box a {
-            margin: 0px 0 20px 0!important;
-    } 
-.yellowbrickbtn button {    
-    padding-left: 10px!important;
-    padding-right: 10px!important;
-}
-}
+            margin: 0px 0 20px 0 !important;
+        }
+
+        .yellowbrickbtn button {
+            padding-left: 10px !important;
+            padding-right: 10px !important;
+        }
+    }
+
     .hooper-slide {
         width: 100% !important;
     }
-.yellowbrickbtn button:hover {
-    color:#000;
-    background: #ffffff;    
+
+    .yellowbrickbtn button:hover {
+        color: #000;
+        background: #ffffff;
         padding-left: 28px;
-    padding-right: 28px;
-}
-.yellowbrickbtn button {    
-    padding-left: 28px;
-    padding-right: 28px;
-}
+        padding-right: 28px;
+    }
+
+    .yellowbrickbtn button {
+        padding-left: 28px;
+        padding-right: 28px;
+    }
+
     .hero {
         background-image: url("/uploads/images/train-tunnel.jpg");
         background-repeat: no-repeat;
@@ -101,10 +106,12 @@
     .announcement-box .announcement-box__card.cant-wait .bg-container {
         background-image: url(/uploads/images/answers-bg.jpg);
     }
+
     .announcement-box a {
-       margin: 20px;
-        box-shadow: 1px 1px 10px 1px rgb(0 0 255 / 14%);
+        margin: 20px;
+        border: 1px solid #e5e5e5;
     }
+
     .announcement-box .arrow-only-white {
         flex-direction: row;
         position: relative;
@@ -221,9 +228,11 @@
         padding: 0;
         list-style: none;
     }
+
     .hover-text:hover, .hover-text {
         padding: 10px;
     }
+
     .hooper-progress {
         position: absolute;
         top: 0;
@@ -238,9 +247,11 @@
         background-color: #4285f4;
         transition: 300ms;
     }
+
     .quote1 {
         font-size: 1.25rem;
     }
+
     .hooper-pagination {
         position: absolute;
         bottom: -30px;
@@ -261,6 +272,7 @@
     .hooper-indicator:hover, .hooper-indicator.is-active {
         background-color: #ffcd32;
     }
+
     .hooper-indicator {
         margin: 0 2px;
         width: 12px;
@@ -271,10 +283,12 @@
         background-color: #fff;
         cursor: pointer;
     }
+
     .hooper-slide.is-active.is-current {
-        background: rgba(128,128,128,0.36078); 
-        border-radius: 10px; 
+        background: rgba(128,128,128,0.36078);
+        border-radius: 10px;
     }
+
     .hooper-pagination.is-vertical {
         bottom: auto;
         right: 0;
@@ -359,7 +373,6 @@
         width: 100%;
         height: 250px;
         height: min-content;
- 
     }
 
         .hooper * {
@@ -434,21 +447,21 @@
     .st1 {
         fill: #FDBF14;
     }
+
     .hooper .rfs-text-base {
-        margin-top:5px;
-        margin-bottom:0px;
-    }  
+        margin-top: 5px;
+        margin-bottom: 0px;
+    }
 </style>
 <template>
     <Layout>
-        <base-section :classNames="'hero flex pt-24 pb-12 md:pt-24 md:pb-10 px-5 xl:px-0'">
+        <base-section :classNames="'hero flex pt-24 pb-12 md:pt-20 md:pb-10 px-5 xl:px-0'">
             <div class="w-full lg:w-2/3 z-10">
-                <h2 class="rfs-text-4xl font-normal text-white">Run your data anywhere across private cloud</h2>
-                <h3 class="rfs-text-3xl text-white sub-title text-yellow1 mb-6 pt-1">Public cloud, on premise, and at the edge, in a single management view.</h3>
-            </div>          
-            <div class="flex flex-col w-full md:max-w-1200 mx-auto">
+                <h3 class="rfs-text-3xl text-white sub-title text-yellow1 mb-6 md:pt-16">Run your data anywhere across private cloud, Public cloud, on premise, and at the edge, in a single management view.</h3>
+            </div>
+            <div class="flex flex-col w-full md:max-w-1200 mx-auto lg:pt-6">
                 <div class="z-10 relative lg:w-5/6">
-                    <hooper ref="carousel" @slide="updateCarousel" :autoPlay="true" :playSpeed="5000" > 
+                    <hooper ref="carousel" @slide="updateCarousel" :autoPlay="true" :playSpeed="10000">
                         <slide>
                             <div class="flex flex-col px-12 pt-12 bg-transparent">
                                 <div class="md:flex">
@@ -457,16 +470,17 @@
                                     </div>
                                     <div class="w-4/5">
                                         <p class="rfs-text-lg mb-1 quote1">
-                                             “We are confident that Yellowbrick’s ability to quickly
+                                            “We are confident that Yellowbrick’s ability to quickly
                                             analyze large amounts of data and offer new insights will help
-                                            us deliver on our goals.”</p><p class="rfs-text-base font-bold">
-                                                Ian Smith, CEO, BMW Group Financial Services North
-                                                America
-                                            </p>
+                                            us deliver on our goals.”
+                                        </p><p class="rfs-text-base font-bold">
+                                            Ian Smith, CEO, BMW Group Financial Services North
+                                            America
+                                        </p>
                                     </div>
                                 </div>
                             </div>
-                        </slide> 
+                        </slide>
                         <slide>
                             <div class="flex flex-col px-12 pt-12 bg-transparent">
                                 <div class="md:flex">
@@ -480,10 +494,10 @@
                                             of data points in real time.”
                                         </p><p class="rfs-text-base font-bold">
                                             Jonathan Ozark, EVP Chief Strategy and Analytics
-                                            Officer, Melco Resorts & Entertainment 
+                                            Officer, Melco Resorts & Entertainment
                                         </p>
                                     </div>
-                                </div>                              
+                                </div>
                             </div>
                         </slide>
                         <slide>
@@ -498,11 +512,12 @@
                                             the data and proprietary algorithms, will produce insights
                                             that were impossible before.”
                                         </p><p class="rfs-text-base font-bold">
-                                            Atul Jain, Chairman & CEO</p>
+                                            Atul Jain, Chairman & CEO
+                                        </p>
                                     </div>
-                                    </div>
+                                </div>
                             </div>
-                      </slide>
+                        </slide>
                         <slide>
                             <div class="flex flex-col px-12 pt-12 bg-transparent">
                                 <div class="md:flex">
@@ -520,7 +535,7 @@
                                     </div>
                                 </div>
                             </div>
-                      </slide>
+                        </slide>
                         <slide>
                             <div class="flex flex-col px-12 pt-12 bg-transparent">
                                 <div class="md:flex">
@@ -537,7 +552,7 @@
                                             Nigel Pratt, SVP Development
                                         </p>
                                     </div>
-                                </div>                              
+                                </div>
                             </div>
                         </slide>
                         <slide>
@@ -556,30 +571,32 @@
                                             Michael Bailey, CTO, Catalina
                                         </p>
                                     </div>
-                                </div>                               
+                                </div>
                             </div>
                         </slide>
                         <hooper-pagination slot="hooper-addons"></hooper-pagination>
                     </hooper>
                     <br />
-                    <div class="z-10">
+                    <div class="z-10 lg:pt-6 md:pb-6">
                         <div class="block sm:flex yellowbrickbtn">
-                            <a href="/why-yellowbrick/"><button class="hover-text text-black px-10 rfs-text-lg bg-yellow1 mt-6">
-                                How Yellowbrick is Different
-                            </button></a>
+                            <a href="/why-yellowbrick/">
+                                <button class="hover-text text-black px-10 rfs-text-lg bg-yellow1 mt-6">
+                                    How Yellowbrick is Different
+                                </button>
+                            </a>
 
                         </div>
                     </div>
                 </div>
             </div>
-            
+
         </base-section>
 
-        
+
         <base-section>
-                <h4 class="rfs-text-4xl mb-5 max-w-screen-md font-bold mt-0" style="padding-left:20px">
-                    in the News
-                </h4>
+            <h4 class="rfs-text-4xl mb-5 max-w-screen-md font-bold mt-0" style="padding-left:20px">
+                In the News
+            </h4>
 
             <div class="w-full flex flex-col md:flex-row announcement-box">
                 <a href="/go/economic-benefits-of-yellowbrick/" class="banner-left flex flex-col w-full md:w-1/3 relative md:border-white">
@@ -593,8 +610,8 @@
                                     ESG Report
                                 </h5>
                                 <p class="font-normal" style="margin-top:5px">Yellowbrick delivers improved and predictable performance and cost with improved operational simplicity and deployment.</p>
-                                </div>
                             </div>
+                        </div>
                     </div>
                 </a>
                 <a :href="$page.home.bannerLeft.url" class="banner-right flex flex-col w-full md:w-1/3 relative md:border-white">
@@ -611,9 +628,9 @@
                                 <p class="font-normal" style="margin-top:5px">
                                     {{ $page.home.bannerLeft.title }}
                                 </p>
-                                </div>
                             </div>
                         </div>
+                    </div>
                 </a>
                 <a :href="$page.home.bannerRight.url" class="banner-right flex flex-col w-full md:w-1/3 relative  md:border-white">
                     <div class="flex flex-col w-full h-full justify-center md:align-center announcement-box__card bmw-group">
@@ -629,9 +646,9 @@
                                 <p class="font-normal" style="margin-top:5px">
                                     {{ $page.home.bannerRight.title }}
                                 </p>
-                                </div>
                             </div>
                         </div>
+                    </div>
                 </a>
             </div>
         </base-section>
